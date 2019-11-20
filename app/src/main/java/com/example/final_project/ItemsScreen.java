4cd19f40476577b7ef426ac94f88f44a11f81a8d
Shayne-G <gibbs.shayne@gmail.com>
2019-11-19 18:54:03 -0600

    Created the ItemsScreen.java to go with the layout file

---
+++ app/src/main/java/com/example/final_project/ItemsScreen.java
@@ -0,0 +1,15 @@
+package com.example.final_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.items_screen_activity);
+
+    }
+}
