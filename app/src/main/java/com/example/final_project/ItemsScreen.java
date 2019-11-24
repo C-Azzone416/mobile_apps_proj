2c3edeca62f64d39dba0a9a055d215623cedb219
Shayne-G <gibbs.shayne@gmail.com>
2019-11-23 19:35:29 -0600

    Built out the first version of adding items to the main list. Reads and writes the items to a file to save them for later, this will likely all be changed. Created FileHelper.java for that operation.

--- app/src/main/java/com/example/final_project/ItemsScreen.java
+++ app/src/main/java/com/example/final_project/ItemsScreen.java
@@ -1,15 +1,69 @@
 package com.example.final_project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ItemsScreen extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+
+public class ItemsScreen extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    private TextInputEditText itemET;
+    private MaterialButton addBtn;
+    private ListView itemsList;
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.items_screen_activity);
 
+        itemET = findViewById(R.id.item_edit_text);
+        addBtn = findViewById(R.id.add_item_button);
+        itemsList = findViewById(R.id.items_list);
+
+        items = FileHelper.readData(this);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        itemsList.setAdapter(adapter);
+
+        addBtn.setOnClickListener(this);
+
+        itemsList.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.add_item_button:
+                String itemEntered = itemET.getText().toString();
+                adapter.add(itemEntered);
+                itemET.setText("");
+
+                FileHelper.writeData(items, this);
+
+                Toast.makeText(this, "Item Added", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        items.remove(position);
+        adapter.notifyDataSetChanged();
+        FileHelper.writeData(items, this);
+        Toast.makeText(this, "Deleted", Toast.LENGTH_SHORT).show();
     }
 }
